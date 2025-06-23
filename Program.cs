fad62973df7a151a5320c837eb96fa7c3b3b21b4
pahanexe <paha.oleinik@gmail.com>
2025-06-23 19:56:29 +0300

    bor working

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Extensions.Configuration;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+using Telegram.Bot.Polling;
+using TGBot.Source;
+
+
+
+namespace TGBot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appconfig.json", optional: false)
+            .Build();
+
+
+            var token = config["TelegramBotToken"];
+
+            if (string.IsNullOrEmpty(token))
+            {
+                Console.WriteLine("appconfig.json is empty");
+                return;
+            }
+
+
+            var bot = new Tgbot(token);
+            bot.Start();
+
+
+            Console.ReadLine();
+        }
+
+    }
+}
---
+++ Source/Tgbot.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot.Exceptions;
+using Telegram.Bot.Types.Enums;
+
+namespace TGBot.Source
+{
+
+    internal class Tgbot
+    {
+        private readonly TelegramBotClient _botClient;
+
+        public Tgbot(string token)
+        {
+            _botClient = new TelegramBotClient(token);
+        }
+        public void Start()
+        {
+            Console.WriteLine("Бот запущено...");
+
+            var receiverOptions = new ReceiverOptions
+            {
+                AllowedUpdates = Array.Empty<UpdateType>() // отримувати всі типи оновлень
+            };
+
+            _botClient.StartReceiving(
+                HandleUpdateAsync,
+                HandleErrorAsync,
+                receiverOptions: receiverOptions,
+                cancellationToken: default
+            );
+        }
+        private async Task HandleUpdateAsync(ITelegramBotClient bot, Update update, CancellationToken token)
+        {
+            if (update.Message is { Text: { } messageText })
+            {
+                long chatId = update.Message.Chat.Id;
+
+                Console.WriteLine($"Отримано повідомлення: {messageText}");
+
+                // await bot.SendMessage
+                await bot.SendMessage(
+                    chatId: chatId,
+                    text: $"Ти написав: {messageText}",
+                    cancellationToken: token
+                );
+            }
+        }
+        private Task HandleErrorAsync(ITelegramBotClient bot, Exception ex, CancellationToken token)
+        {
+            var errorMessage = ex switch
+            {
+                ApiRequestException apiEx => $"Telegram API Error:\n[{apiEx.ErrorCode}] {apiEx.Message}",
+                _ => ex.ToString()
+            };
+
+            Console.WriteLine($"❌ {errorMessage}");
+            return Task.CompletedTask;
+        }
+    }
+}
